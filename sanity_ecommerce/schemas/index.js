b20248d7e3be4254e45968cb469bd2f147a78456
Hafsa Irfan <73380074+HafsaI@users.noreply.github.com>
2023-02-12 21:50:08 +0500

    sanity folder

---
+++ sanity_ecommerce/schemas/index.js
@@ -0,0 +1,17 @@
+// schemas/index.js
+import product from './product'
+
+
+export const schemaTypes = [product]
+// import createSchema from 'part:@sanity/base/schema-creator';
+// import schemaTypes from 'all:part:@sanity/base/schema-type';
+
+// import product from './product';
+// import banner from './banner';
+
+// export default createSchema({
+//   name: 'default',
+//   types: schemaTypes.concat([ product, banner ]),
+// })
+  
+ 
